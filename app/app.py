57c69d7e508d7fcb245230250a0d6b235b572a56
Ziqian Zhang <ziqian1@ad.unc.edu>
2025-12-02 15:20:57 -0500

    Added the voting scatterplot

--- app/app.py
+++ app/app.py
@@ -20,13 +20,31 @@ dbms = 'postgresql'
 package = 'psycopg'
 user = 'postgres'
 password = POSTGRES_PASSWORD
-host = 'localhost'
+host = 'postgres'
 port = '5432'
 db = 'contrans'
 
 engine = create_engine(f'{dbms}+{package}://{user}:{password}@{host}:{port}/{db}')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# Create features that we need, but won't change depending on what the user does
+myquery = '''
+SELECT *
+FROM members
+'''
+data = pd.read_sql_query(myquery, con=engine)
+data2 = data[['full_name', 'state_abbrev', 'district_code', 'party']]
+data2['party'] = data2['party'].str[0]
+display_name = [n + ' (' + p + ', ' + s + '-' + str(d) + ')'
+                for n, s, d, p in 
+                zip(data2['full_name'], 
+                    data2['state_abbrev'],
+                    data2['district_code'],
+                    data2['party'])]
+display_name = [x.replace('-0', '') for x in display_name]
+
+dropdown_options = [{'label': y, 'value': x} for x, y in zip(data['bioguide_id'], display_name)]
 
 # Define the Dash app
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
@@ -37,16 +55,26 @@ app.layout = html.Div(
         html.H1('Know Your Representatives in Congress!'),
 
         html.Div([
-            dcc.Markdown('Dropdown goes here')
+            dcc.Markdown('''If you know your Representative or Senators, 
+                         select them in the dropbox box below. If you want 
+                         to lookup who your representatives are, check out 
+                         [this website](https://www.congress.gov/members/find-your-member).'''),
+            dcc.Dropdown(id = 'dropdown', options=dropdown_options, 
+                         value='N000188') # Step 1: User input
         ],
                  style={'width':'25%', 'float':'left'}),
 
         html.Div([
             dcc.Tabs([
                 dcc.Tab(label = 'Biographical Information',
-                        children = []),
+                        children = [
+                            html.Div([html.Img(id = 'bioimage')], style={'width':'20%', 'float':'left'}),
+                            html.Div([dcc.Graph(id = 'biotable')], style={'width':'80%', 'float':'right'})
+                        ]),
                 dcc.Tab(label = 'How They Vote',
-                        children = []),
+                        children = [
+                            dcc.Graph(id='vote_scatter')
+                        ]),
                 dcc.Tab(label = 'Sponsored Bills',
                         children = []),
                 dcc.Tab(label = 'Who is Giving Them Money?',
@@ -62,9 +90,63 @@ app.layout = html.Div(
 
 # Define the 'callbacks' -- user input -> output functions
 
+@app.callback([Output(component_id = 'biotable', component_property='figure')], # Output() is step 4 
+              [Input(component_id = 'dropdown', component_property = 'value')]) # Input() is step 2
+
+def biotable(b): # Step 3: write a function that turns input into output
+    myquery = f'''
+        SELECT *
+        FROM members
+        WHERE bioguide_id = '{b}'
+    '''
+    member_info = pd.read_sql_query(myquery, con=engine)
+    member_info = member_info.drop(['bioguide_id', 'image', 'fec_id', 'bioname', 'icpsr'],
+        axis=1)
+    return [ff.create_table(member_info.T.reset_index().rename({'index':'',0:''}, axis=1))]
+
+@app.callback([Output(component_id = 'bioimage', component_property='src')], 
+              [Input(component_id = 'dropdown', component_property = 'value')])
+
+def bioimage(b):
+    
+    myquery = f'''
+        SELECT image
+        FROM members
+        WHERE bioguide_id = '{b}'
+    '''
+    image_url = pd.read_sql_query(myquery, con=engine)['image'][0]
+    return [image_url]
+
+@app.callback([Output(component_id = 'vote_scatter', component_property='figure')], 
+              [Input(component_id = 'dropdown', component_property = 'value')])
+
+def vote_scatterplot(b):
+    myquery = f'''
+        SELECT c.comparison_member,
+            c.agree,
+            m.left_right_ideology,
+            m.party
+        FROM members m
+        INNER JOIN (
+            SELECT vc.comparison_member,
+                vc.agree
+            FROM members m
+            INNER JOIN vote_compare vc
+                ON m.bioname = vc.bioname
+            WHERE m.bioguide_id = '{b}'
+        ) c
+            ON m.bioname = c.comparison_member
+
+        '''
+    vote_data = pd.read_sql_query(myquery, con=engine)
+    fig = px.scatter(vote_data,
+                 x = 'left_right_ideology',
+                 y = 'agree',
+                 hover_name = 'comparison_member',
+                 color = 'party',
+                 color_discrete_map={'Democrat': 'blue','Republican': 'red'})
+    return [fig]
+
 # Run the dashboard
 if __name__=='__main__':
     app.run(debug=True, host='0.0.0.0', port=8050)
-
-
-
--- contrans.py
+++ contrans.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+pd.options.mode.copy_on_write = True
 import requests
 import json
 import dotenv
 import os
+import time
 
 class contrans:
 
@@ -15,6 +17,11 @@ class contrans:
         self.MONGO_INITDB_ROOT_PASSWORD = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
         self.congresskey = os.getenv('congresskey')
         self.feckey = os.getenv('feckey')
+        self.botname = 'contrans'
+        self.version = '0.0'
+        self.github = 'https://github.com/jkropko/contrans2025'
+        self.useragent = f'{self.botname}/{self.version} ({self.github}) python-requests/{requests.__version__}'
+        self.headers = {'User-Agent': self.useragent}
 
 # build crosswalk with ideology
 
@@ -77,6 +84,263 @@ class contrans:
         
 # get biodata
 
+    def get_bio_data(self, bioguide_id):
+        root = 'https://api.congress.gov/v3'
+        endpoint = f'/member/{bioguide_id}'
+
+        params = {'format': 'json', 'api_key': self.congresskey} 
+
+        r = requests.get(root + endpoint, headers = self.headers, params = params)
+        myjson = json.loads(r.text)['member']
+        terms = myjson['terms'] 
+        terms = pd.DataFrame(terms)
+        terms['bioguide_id'] = bioguide_id
+        try:
+            terms['endYear'] = terms['endYear'].fillna(2027).astype(int)
+        except: 
+            terms['endYear'] = 2027
+        termdata = terms[['bioguide_id','chamber', 'congress', 'stateCode', 'startYear', 'endYear']]
+        try:
+            termdata['district'] = terms['district']
+        except:
+            termdata['district'] = None
+        member = {
+            'bioguide_id': myjson['bioguideId'],
+            'Full name':myjson['directOrderName'],
+            'Chamber': myjson['terms'][-1]['chamber'],
+            'State': myjson['state'],
+            'Party': myjson['partyHistory'][-1]['partyName']}
+        try:
+            member['District'] = myjson['district']
+        except:
+            member['District'] = None
+        try:
+            member['birthYear'] = myjson['birthYear']
+        except:
+            pass
+        try:
+            member['image'] = myjson['depiction']['imageUrl']
+        except:
+            pass
+        try:
+            member['Office address'] = f'{myjson["addressInformation"]["officeAddress"]}, {myjson["addressInformation"]["city"]}, {myjson["addressInformation"]["district"]} {myjson["addressInformation"]["zipCode"]}'
+            member['Phone'] = myjson['addressInformation']['phoneNumber']
+            member['Website'] = myjson['officialWebsiteUrl']
+        except:
+            pass
+        return termdata, member   
+    
+    def save_bio_terms(self):
+        ideology = pd.read_csv('data/ideology.csv')
+        bioguide_ids = ideology['bioguide_id'].unique()
+        memberlist = []
+        termslist = []
+        i = 0
+        for bioguide_id in bioguide_ids:
+            if i % 10 == 0:
+                print(f'Now uploading legislator {i} ({bioguide_id}) of {len(bioguide_ids)}')
+            terms, member = self.get_bio_data(bioguide_id)
+            termslist.append(terms)
+            memberlist.append(member)
+            i += 1
+        member = pd.DataFrame(memberlist)
+        terms = pd.concat(termslist)
+        member.to_csv(f'data/bioinfo.csv', index=False)
+        terms.to_csv(f'data/terms.csv', index=False)
+
 # get sponsored legislation
+    def get_sponsored_legislation_member(self, bioguide_id):
+        root = 'https://api.congress.gov/v3'
+        endpoint = f'/member/{bioguide_id}/sponsored-legislation'
+        params = {'format': 'json',
+          'offset': 0,
+          'limit': 250,
+          'api_key': self.congresskey}
+        r = requests.get(root + endpoint, headers = self.headers, params = params)
+        myjson = json.loads(r.text)
+        s = [x for x in myjson['sponsoredLegislation'] if x['congress'] == 119]
+        s = [{k: v for k, v in x.items() if k in ['introducedDate', 'type', 'number', 'title', 'url']} for x in s]
+        spons = pd.DataFrame(s)
+        spons['bioguide_id'] = bioguide_id
+        return spons
+    
+    def get_sponsored_legislation(self):
+        sl_list = []
+        ideology = pd.read_csv('data/ideology.csv')
+        bioguide_ids = ideology['bioguide_id'].unique()
+        i = 0
+        for bioguide_id in bioguide_ids:
+            if i % 10 == 0:
+                print(f'Now uploading legislator {i} ({bioguide_id}) of {len(bioguide_ids)}')
+            spons = self.get_sponsored_legislation_member(bioguide_id)
+            sl_list.append(spons)
+            i += 1
+        spons = pd.concat(sl_list)
+        spons.to_csv(f'data/sponsored_legislation.csv', index=False)
+
+    def get_bill_summaries(self, congress=119):
+        root = 'https://api.congress.gov/v3'
+        endpoint = f'/summaries/{congress}'
+        params = {'format': 'json',
+          'offset': 0,
+          'limit': 250,
+          'api_key': self.congresskey,
+          'fromDateTime':'2025-01-01T00:00:00Z',
+          'toDateTime':'2025-10-21T00:00:00Z'
+          }
+        r = requests.get(root + endpoint, headers = self.headers, params = params)
+        myjson = json.loads(r.text)
+        sum_list = [pd.json_normalize(myjson['summaries'])]
+        l = len(sum_list)
+        i = 250
+        while l > 0:
+            print(i)
+            params['offset'] = i
+            r = requests.get(root + endpoint, headers = self.headers, params = params)
+            myjson = json.loads(r.text)
+            sum_list = sum_list + [pd.json_normalize(myjson['summaries'])]
+            l = len(myjson['summaries'])
+            i += 250
+        summaries = pd.concat(sum_list)
+        summaries.to_csv(f'data/bill_summaries.csv', index=False)
 
 # get donors
+    def get_fec_key_member(self, bioguide_id):
+        ideology = pd.read_csv('data/ideology.csv')
+        memberdata = ideology.query(f"bioguide_id=='{bioguide_id}'").reset_index()
+        params = {'api_key': self.feckey,
+          'q': memberdata['bioname'][0].split(',')[0],
+          'state': memberdata['state_abbrev'][0],
+          'year': '2024'}
+        if memberdata['district_code'][0] > 0:
+            params['district'] = str(memberdata['district_code'][0].astype(object))
+            params['office'] = 'H'
+        else:
+            params['office'] = 'S'
+        
+        r = requests.get('https://api.open.fec.gov/v1/candidates/search/', 
+                         params = params,
+                         headers = self.headers)
+        myjson = json.loads(r.text)
+        try:
+            try:
+                mykeys = {'bioguide_id': bioguide_id,
+                        'fec_id': myjson['results'][0]['candidate_id']}
+            except:
+                del params['district']
+                r = requests.get('https://api.open.fec.gov/v1/candidates/search/', 
+                            params = params,
+                            headers = self.headers)
+                myjson = json.loads(r.text)
+                mykeys = {'bioguide_id': bioguide_id,
+                        'fec_id': myjson['results'][0]['candidate_id']}
+        except:
+            mykeys = {'bioguide_id': bioguide_id,
+                    'fec_id': None}
+        return mykeys
+
+    
+    def get_fec_keys(self):
+        fec_list = []
+        try:
+            fec_ids = pd.read_csv('data/fec_ids.csv')
+        except:
+            fec_ids = pd.read_csv('data/ideology.csv')
+            fec_ids = fec_ids[['bioguide_id']]
+            fec_ids['fec_id'] = None
+
+        fec_ids_nonmissing = fec_ids[fec_ids['fec_id'].notna()]
+        fec_ids_missing = fec_ids[fec_ids['fec_id'].isna()]
+
+        bioguide_ids = fec_ids_missing['bioguide_id'].unique()
+ 
+        i = 0
+        for bioguide_id in bioguide_ids:
+            time.sleep(1)
+            if i % 1 == 0:
+                print(f'Now uploading legislator {i} ({bioguide_id}) of {len(bioguide_ids)}')
+            feckey = self.get_fec_key_member(bioguide_id)
+            fec_list.append(feckey)
+            i += 1
+        fecids = pd.DataFrame(fec_list)
+        fecids = pd.concat([fec_ids_nonmissing, fecids])
+        fecids.to_csv(f'data/fec_ids.csv', index=False)
+
+
+    def get_contrib_committees(self, fec_id):
+        root = 'https://api.open.fec.gov'
+        endpoint = f'/v1/candidate/{fec_id}/committees/'
+        params = {'api_key': self.feckey, 'cycle': '2024'}
+        r = requests.get(root + endpoint, params=params, headers=self.headers)
+        while r.status_code != 200:
+            time.sleep(10)
+            r = requests.get(root + endpoint, params=params, headers=self.headers)
+        committee_ids = [x['committee_id'] for x in r.json()['results']]
+        return committee_ids
+
+    def get_member_contributions(self, fec_id):
+
+        root = 'https://api.open.fec.gov'
+        endpoint = '/v1/schedules/schedule_a/'
+
+        committee_ids = self.get_contrib_committees(fec_id)
+        contrib_list = []
+        for c in committee_ids:
+            
+            params = {'api_key': self.feckey,
+                      'committee_id': c,
+                      'per_page': 100,
+                      'sort': '-contribution_receipt_amount'}
+
+            r = requests.get(root + endpoint, params=params, headers=self.headers)
+
+            contrib_list = contrib_list + [{'contributor_name': x['contributor_name'],
+                            'contributor_aggregate_ytd': x['contributor_aggregate_ytd'],
+                            'memo_text': x['memo_text'],
+                            'pdf_url': x['pdf_url']} for x in r.json()['results']]
+            lastindex = r.json()['pagination']['last_indexes']
+
+            newrecords = len(r.json()['results'])
+
+            while newrecords > 0:
+                print(len(contrib_list))
+
+                params['last_contribution_receipt_amount']=lastindex['last_contribution_receipt_amount']
+                params['last_index']=lastindex['last_index']
+
+                r = requests.get(root + endpoint, params=params,headers=self.headers)
+                while r.status_code != 200:
+                    print(r.text)
+                    time.sleep(10)
+                    continue
+
+                contrib_list = contrib_list + [{'contributor_name': x['contributor_name'],
+                            'contributor_aggregate_ytd': x['contributor_aggregate_ytd'],
+                            'memo_text': x['memo_text'],
+                            'pdf_url': x['pdf_url'],
+                            'fec_committee_id': c} for x in r.json()['results']]
+                
+                lastindex = r.json()['pagination']['last_indexes']
+
+                newrecords = len(r.json()['results'])
+
+
+        contrib_df = pd.DataFrame(contrib_list)
+        contrib_df['fec_id'] = fec_id
+        return contrib_df
+
+    def get_all_contributions(self):
+        fec_ids = pd.read_csv('data/fec_ids.csv')
+        contrib_df = pd.read_csv('data/contrib.csv')
+        fid = fec_ids[fec_ids['fec_id'].notna()]['fec_id'].unique()
+        already_have = contrib_df['fec_id'].unique()
+        need_to_get = np.setdiff1d(fid, already_have)
+        print(f'{len(need_to_get)} legislators still to upload')
+        i = 1
+        for f in need_to_get:
+            print(f'Now uploading legislator {i} ({f}) of {len(need_to_get)}')
+            newdata = self.get_member_contributions(f)
+            contrib_df = pd.concat([contrib_df, newdata])
+            contrib_df.to_csv(f'data/contrib.csv', index=False)
+            i += 1
+            
